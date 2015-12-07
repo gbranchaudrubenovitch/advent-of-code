f83a207dafe84f1722af4db517cc49e4261d9c4c
Gabriel Branchaud Rubenovitch <gabriel.branchaud@gmail.com>
2015-12-07 00:59:13 -0500

    minor typo.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var d02 = require("./src/day-02/solver");
 console.time("challenges took");
 console.log("**************ADVENT OF CODE**************");
 console.log("  --- Day 1: Not Quite Lisp (1/2) - " + d01.solvePartOne());
-console.log("  --- Day 1: Not Quite Lisp (1/2) - " + d01.solvePartTwo());
+console.log("  --- Day 1: Not Quite Lisp (2/2) - " + d01.solvePartTwo());
 console.log("  --- Day 2: I Was Told There Would Be No Math (1/2) - " + d02.solvePartOne());
 
 console.timeEnd("challenges took");

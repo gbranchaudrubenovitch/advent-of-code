02461b0abe679947125e5fe860f8ed0d969e6714
Gabriel Branchaud Rubenovitch <gabriel.branchaud@gmail.com>
2015-12-24 13:44:29 -0500

    pseudo-code on day 9.

---
+++ src/day-09/route-generator.js
@@ -0,0 +1,6 @@
+"use strict";
+
+exports.all = function all(roadSegments) {
+  // TODO: generate all routes that pass through all nodes in graph
+  return [];
+};
--- src/day-09/traveling-santa.js
+++ src/day-09/traveling-santa.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var cityGraphBuilder = require("./city-graph-builder");
+var routeGenerator = require("./route-generator");
 
 module.exports = class TravelingSanta {
   constructor(roadSegments) {
@@ -9,6 +10,22 @@ module.exports = class TravelingSanta {
   }
 
   computeLengthOfShortestRouteAcrossAllCities() {
+    let currentShortestRoute = Number.MAX_VALUE;
+
+    let allRoutes = routeGenerator.all(this.roadSegments);
+    for(let route of allRoutes) {
+      let routeLength = this.computeLengthOfRoute(route);
+      if (routeLength < currentShortestRoute) {
+        currentShortestRoute = routeLength;
+      }
+    }
+
+    // HACK!
     return this.roadSegments[0].distance;
+  }
+
+  computeLengthOfRoute(route) {
+    // TODO: do this
+    return -1;
   }
 };

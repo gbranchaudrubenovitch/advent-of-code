e9d5b642070f052457e7a1bb1dd87ef7f102fb59
Gabriel Branchaud Rubenovitch <gabriel.branchaud@gmail.com>
2015-12-25 11:04:18 -0500

    day 9, part1 solved.

--- src/day-09/solver.js
+++ src/day-09/solver.js
@@ -1,9 +1,13 @@
 "use strict";
 
+let TravelingSanta = require("./traveling-santa");
+let segments = require("./segments");
 let readAllLines = require("../helpers/resources").readAllLines;
 
+let travelingSanta = new TravelingSanta(segments.fromStrings(readAllLines("day-9-input.txt")));
+
 exports.solvePartOne = () => {
-  return "TODO";
+  return "The distance of the shortest route is " + travelingSanta.computeLengthOfShortestRouteAcrossAllCities();
 };
 
 exports.solvePartTwo = () => {
--- src/day-09/traveling-santa.js
+++ src/day-09/traveling-santa.js
@@ -6,26 +6,53 @@ var routeGenerator = require("./route-generator");
 module.exports = class TravelingSanta {
   constructor(roadSegments) {
     this.cityGraph = cityGraphBuilder.build(roadSegments);
-    this.roadSegments = roadSegments;
+    this.depth = this.cityGraph.nodes().length;
   }
 
   computeLengthOfShortestRouteAcrossAllCities() {
     let currentShortestRoute = Number.MAX_VALUE;
 
-    let allRoutes = routeGenerator.all(this.roadSegments);
-    for(let route of allRoutes) {
+    let allRoutes = this.generateAllRoutes();
+    for (let route of allRoutes) {
       let routeLength = this.computeLengthOfRoute(route);
-      if (routeLength < currentShortestRoute) {
-        currentShortestRoute = routeLength;
-      }
+      currentShortestRoute = Math.min(routeLength, currentShortestRoute);
+    }
+    return currentShortestRoute;
+  }
+
+  generateAllRoutes() {
+    let allRoutes = [];
+    let cities = this.cityGraph.nodes();
+    for (let startingCity of cities) {
+      let routes = this.generateRoutes(startingCity, [startingCity]);
+      allRoutes = allRoutes.concat(routes);
+    }
+    return allRoutes;
+  }
+
+  generateRoutes(startingCity, alreadyVisitedCities) {
+    if (alreadyVisitedCities.length === this.depth) {
+      return [alreadyVisitedCities];
     }
 
-    // HACK!
-    return this.roadSegments[0].distance;
+    let routes = [];
+    let neighbors = this.cityGraph.neighbors(startingCity);
+    for (let neighbor of neighbors) {
+      if (alreadyVisitedCities.includes(neighbor)) {
+        continue;
+      }
+      routes = routes.concat(this.generateRoutes(neighbor, alreadyVisitedCities.concat(neighbor)));
+    }
+    return routes;
   }
 
   computeLengthOfRoute(route) {
-    // TODO: do this
-    return -1;
+    var length = 0;
+    for (var i = 0; i < route.length - 1; i++) {
+      var fromCity = route[i];
+      var toCity = route[i + 1];
+      length += this.cityGraph.edge(fromCity, toCity);
+    }
+    return length;
   }
 };

f46709c8e5f27198d7e506956e171b44b9bce221
Gabriel Branchaud Rubenovitch <gabriel.branchaud@gmail.com>
2016-01-14 22:23:26 -0500

    Day 16, part 1 solved.

--- src/day-16/sue-finder.js
+++ src/day-16/sue-finder.js
@@ -2,7 +2,24 @@
 
 let sueParser = require("./sue-parser");
 
+let candidateIsAMatch = (candidateSue, compoundsTheRightSueMustMatch) => {
+  for(let compoundName in compoundsTheRightSueMustMatch) {
+    let candidateValue = candidateSue[compoundName];
+    let expectedValue = compoundsTheRightSueMustMatch[compoundName];
+
+    if (candidateValue !== undefined && candidateValue !== expectedValue) {
+      return false;
+    }
+  }
+  return true;
+};
+
 exports.findTheRightSue = (rawSues, compoundsTheRightSueMustMatch) => {
   let allSues = sueParser.from(rawSues);
-  return allSues[0].number;
+  for(let candidateSue of allSues) {
+    if (candidateIsAMatch(candidateSue, compoundsTheRightSueMustMatch)) {
+      return candidateSue.number;
+    }
+  }
+  throw new Error("could not find the right Sue");
 };
--- test/day-16/sue-finder-test.js
+++ test/day-16/sue-finder-test.js
@@ -2,13 +2,22 @@ var expect = require("chai").expect;
 var sueFinder = require("../../src/day-16/sue-finder");
 
 describe("--- Day 16: (1/2) finding the right Sue --- ", () => {
-  it("finds the number of the only Sue when all compounds match", () => {
+  it("finds the number of the only Sue", () => {
     var theOnlySue = "Sue 22: children: 1, cats: 2, samoyeds: 3";
     var compoundsLeftOnTheGift = compounds(1, 2, 3);
 
     var theRightSueNumber = sueFinder.findTheRightSue([theOnlySue], compoundsLeftOnTheGift);
 
     expect(theRightSueNumber).to.equal(22);
+  });
+
+  it("finds the number of the right Sue", () => {
+    var sues = ["Sue 42: trees: 6, pomeranians: 9, goldfish: 8", "Sue 128: cars: 9, children: 1, cats: 1"];
+    var compoundsLeftOnTheGift = compounds(1, 1, 3, 4, 5, 6, 7, 8, 9, 10);
+
+    var theRightSueNumber = sueFinder.findTheRightSue(sues, compoundsLeftOnTheGift);
+
+    expect(theRightSueNumber).to.equal(128);
   });
 });
 

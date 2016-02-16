41cad81574e13d88c2d4285981c0803ee769a6d3
Gabriel Branchaud <gabriel.branchaud@gmail.com>
2016-02-16 15:14:49 -0500

    Day 21: progress on the loadout generator.

--- src/day-21/loadouts-generator.js
+++ src/day-21/loadouts-generator.js
@@ -5,8 +5,30 @@
 // rings: 5 choose 0 + 5 choose 1 + 5 choose 2 = 1 + 5 + 10 = 16
 // total loadouts = weapons * armor * rings =  5 * 5 * 16 = 400
 
+let newLoadout = () => {
+  return {
+    cost: 0,
+    damage: 0,
+    armor: 0
+  };
+};
+
 exports.generateFrom = (store) => {
-  return [];
+  let loadouts = [];
+  for (let weapon of store.weapons) {
+    for (let armor of store.armors) {
+      let currentLoadout = newLoadout();
+
+      currentLoadout.cost += weapon.cost + armor.cost;
+      currentLoadout.damage += weapon.damage;
+      currentLoadout.armor += armor.armor;
+
+      loadouts.push(currentLoadout);
+    }
+
+  }
+
+  return loadouts;
 };
 
 exports.generateFromDefaultStore = () => {
--- test/day-21/loadouts-generator-test.js
+++ test/day-21/loadouts-generator-test.js
@@ -2,14 +2,27 @@ var expect = require("chai").expect;
 var loadoutsGenerator = require("../../src/day-21/loadouts-generator");
 
 describe("Loadouts generator", () => {
-  it.skip("(WIP) can generate a single loadout", () => {
-    var storeWith1Weapon = store([item("Dagger", 8, 4, 0)], [], []);
-    var loadouts = loadoutsGenerator.generateFrom(storeWith1Weapon);
+  it("can generate a single loadout", () => {
+    var theStore = store([item("Dagger", 8, 4, 0)], [item("Leather", 13, 0, 1)], []);
+    var loadouts = loadoutsGenerator.generateFrom(theStore);
 
     expect(loadouts).to.have.length(1);
-    expect(loadouts[0].cost).to.equal(8);
+    expect(loadouts[0].cost).to.equal(21);
     expect(loadouts[0].damage).to.equal(4);
-    expect(loadouts[0].armor).to.equal(0);
+    expect(loadouts[0].armor).to.equal(1);
+  });
+
+  it("can generate 2 loadouts", () => {
+    var theStore = store([item("Dagger", 8, 4, 0), item("Shortsword", 10, 5, 0)], [item("Leather", 13, 0, 1)], []);
+    var loadouts = loadoutsGenerator.generateFrom(theStore);
+
+    expect(loadouts).to.have.length(2);
+    expect(loadouts[0].cost).to.equal(21);
+    expect(loadouts[0].damage).to.equal(4);
+    expect(loadouts[0].armor).to.equal(1);
+    expect(loadouts[1].cost).to.equal(23);
+    expect(loadouts[1].damage).to.equal(5);
+    expect(loadouts[1].armor).to.equal(1);
   });
 });
 
